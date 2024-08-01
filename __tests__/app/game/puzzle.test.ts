ef5d156be3d7a8077c8114e8ccfb70de406bf32e
Greg Dyer <gregjcdyer@gmail.com>
2024-07-31 21:19:15 -0700

    fix: vs code debugging

--- __tests__/app/game/puzzle.test.ts
+++ __tests__/app/game/puzzle.test.ts
@@ -1,8 +1,9 @@
 import {generatePuzzle} from '../../../app/game/Puzzle';
 
 describe('Puzzle', () => {
-  it.only('generates a puzzle with correct dimensions', () => {
+  it('generates a puzzle with correct dimensions', () => {
     const puzzle = generatePuzzle();
+
     expect(puzzle).toHaveLength(9);
   });
 });
